f72e803148d2f0494b095b33324dd36db31cd4f1
Onno Kampman <onno.kampman@gmail.com>
2020-03-27 18:35:39 +0000

    add data loader

--- nifti_gifs/file_helpers.py
+++ nifti_gifs/file_helpers.py
@@ -1,8 +1,12 @@
+import os
+
 from imageio import mimwrite
+import nibabel as nb
 import numpy as np
+from skimage.transform import resize
 
 
-def save_gif(filename: str, gif_array: np.array, frames_per_second: int, resize_factor) -> None:
+def save_gif(filename: str, gif_array: np.array, frames_per_second: int, resize_factor: float) -> None:
     mimwrite(
         filename,
         gif_array,
@@ -10,3 +14,10 @@ def save_gif(filename: str, gif_array: np.array, frames_per_second: int, resize_
         fps=int(frames_per_second * resize_factor)
     )
     print("Gif saved in %s" % filename)
+
+
+def load_data(filename: str) -> np.array:
+    data = nb.load(filename).get_data()
+    print('data', data.shape)
+    return data
+
